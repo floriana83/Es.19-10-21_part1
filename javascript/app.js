9ff3f56e75eb746b8636221dcec1e15b74013479
Floriana Domianello <floriana.domianello@gmail.com>
2021-10-20 10:11:17 +0200

    esercizio corretto

--- javascript/app.js
+++ javascript/app.js
@@ -1,20 +1,20 @@
 let Animale = prompt("Animale preferito:\n 1. Dog\n 2. Cat\n 3. Goldfish\n 4. Monkey\n 5. Panda");
 
-if (Animale == 1) {
+if (Animale === "1") {
     console.log("Chose the Dog");
 } 
-else if (Animale == 2) {
+else if (Animale === "2") {
     console.log("Chose the Goldfish");
 }
-else if (Animale == 3) {
+else if (Animale === "3") {
     console.log("Chose the Cat");
 }
-else if (Animale == 4) {
+else if (Animale === "4") {
     console.log("Chose the Monkey");
 }
-else if (Animale == 5) {
+else if (Animale === "5") {
     console.log("Chose the Panda");
 }
-else if (Animale > 5) {
+else {
     console.log("Animal not found");
 }
